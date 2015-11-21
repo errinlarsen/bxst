3f30a74ed106057f1ed27d69d93137e71d6185af
Errin Larsen <errinlarsen@gmail.com>
2015-11-20 21:54:17 -0600

    [INIT] Fix provision script bugs

--- provision.sh
+++ provision.sh
@@ -3,26 +3,24 @@
 cd /vagrant
 
 # update the package manager
-apt-get update && sudo -E apt-get upgrade -y
+apt-get update && sudo apt-get upgrade -y
 
 # install git
-sudo -E apt-get install -y git
+sudo apt-get install -y git
 
 # install PostgreSQL (DB)
-sudo -E /usr/sbin/update-locale LANG=en_US.UTF-8 LC_ALL=en_us.UTF-8
-sudo -E apt-get install -y postgresql libpq-dev
+sudo apt-get install -y postgresql libpq-dev
 
 # other stuff
-sudo -E apt-get install -y build-essential curl ruby-dev libxml2 libxml2-dev libxslt1-dev
+sudo apt-get install -y build-essential curl ruby-dev libxml2 libxml2-dev libxslt1-dev
 
 # rbenv (Ruby Version Manager)
 sudo -u vagrant git clone https://github.com/sstephenson/rbenv.git /home/vagrant/.rbenv
-sudo -u vagrant echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> /home/vagrant/.bashrc
-sudo -u vagrant echo 'eval "$(rbenv init -)"' >> /home/vagrant/.bashrc
+sudo -u vagrant echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> /home/vagrant/.profile
+sudo -u vagrant echo 'eval "$(rbenv init -)"' >> /home/vagrant/.profile
 sudo -u vagrant git clone https://github.com/sstephenson/ruby-build.git /home/vagrant/.rbenv/plugins/ruby-build
 
 # ...install a Ruby and the Bundler gem
-source ~/.bashrc
 sudo -u vagrant -i rbenv install 2.2.3
 sudo -u vagrant -i rbenv global 2.2.3
 sudo -u vagrant -i gem install bundler --no-ri --no-rdoc
